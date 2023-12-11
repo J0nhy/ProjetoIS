96058c6fdf00b01f30e8a912f1fbc44675054667
João Custódio <jpsasilva@hotmail.com>
2023-12-11 11:20:36 +0000

    Rename/Create Switch

---
+++ Switch/Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace Switch
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnOn = new System.Windows.Forms.Button();
+            this.btnOff = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnOn
+            // 
+            this.btnOn.Location = new System.Drawing.Point(48, 131);
+            this.btnOn.Name = "btnOn";
+            this.btnOn.Size = new System.Drawing.Size(174, 75);
+            this.btnOn.TabIndex = 0;
+            this.btnOn.Text = "ON";
+            this.btnOn.UseVisualStyleBackColor = true;
+            // 
+            // btnOff
+            // 
+            this.btnOff.Location = new System.Drawing.Point(48, 226);
+            this.btnOff.Name = "btnOff";
+            this.btnOff.Size = new System.Drawing.Size(174, 75);
+            this.btnOff.TabIndex = 1;
+            this.btnOff.Text = "OFF";
+            this.btnOff.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(277, 450);
+            this.Controls.Add(this.btnOff);
+            this.Controls.Add(this.btnOn);
+            this.Name = "Form1";
+            this.Text = "Switch";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnOn;
+        private System.Windows.Forms.Button btnOff;
+    }
+}
+
