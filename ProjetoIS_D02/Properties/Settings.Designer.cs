f23df71e0f9b8371230aec2dfc118f053011e421
João Custódio <jpsasilva@hotmail.com>
2023-12-18 09:56:22 +0000

    InicioSwitch e BD

--- ProjetoIS_D02/Properties/Settings.Designer.cs
+++ ProjetoIS_D02/Properties/Settings.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace Switch.Properties {
+namespace ProjetoIS_D02.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
@@ -26,11 +26,11 @@ namespace Switch.Properties {
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
-        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\DevelopmentIS\\ProjetoIS_D0" +
-            "2\\ProjetoIS_D02\\App_Data\\SOMIOD.mdf;Integrated Security=True;Connect Timeout=30")]
-        public string ConnStr {
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\\SOMIOD.mdf;In" +
+            "tegrated Security=True;Connect Timeout=30")]
+        public string connStr {
             get {
-                return ((string)(this["ConnStr"]));
+                return ((string)(this["connStr"]));
             }
         }
     }
--- Switch/Form1.Designer.cs
+++ Switch/Form1.Designer.cs
@@ -30,35 +30,91 @@
         {
             this.btnOn = new System.Windows.Forms.Button();
             this.btnOff = new System.Windows.Forms.Button();
+            this.textBoxModuleName = new System.Windows.Forms.TextBox();
+            this.textBoxAppName = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // btnOn
             // 
-            this.btnOn.Location = new System.Drawing.Point(48, 131);
+            this.btnOn.Location = new System.Drawing.Point(64, 161);
+            this.btnOn.Margin = new System.Windows.Forms.Padding(4);
             this.btnOn.Name = "btnOn";
-            this.btnOn.Size = new System.Drawing.Size(174, 75);
+            this.btnOn.Size = new System.Drawing.Size(232, 92);
             this.btnOn.TabIndex = 0;
             this.btnOn.Text = "ON";
             this.btnOn.UseVisualStyleBackColor = true;
+            this.btnOn.Click += new System.EventHandler(this.btnOn_Click);
             // 
             // btnOff
             // 
-            this.btnOff.Location = new System.Drawing.Point(48, 226);
+            this.btnOff.Location = new System.Drawing.Point(64, 278);
+            this.btnOff.Margin = new System.Windows.Forms.Padding(4);
             this.btnOff.Name = "btnOff";
-            this.btnOff.Size = new System.Drawing.Size(174, 75);
+            this.btnOff.Size = new System.Drawing.Size(232, 92);
             this.btnOff.TabIndex = 1;
             this.btnOff.Text = "OFF";
             this.btnOff.UseVisualStyleBackColor = true;
             // 
+            // textBoxModuleName
+            // 
+            this.textBoxModuleName.Location = new System.Drawing.Point(190, 50);
+            this.textBoxModuleName.Name = "textBoxModuleName";
+            this.textBoxModuleName.Size = new System.Drawing.Size(141, 22);
+            this.textBoxModuleName.TabIndex = 2;
+            // 
+            // textBoxAppName
+            // 
+            this.textBoxAppName.Location = new System.Drawing.Point(190, 98);
+            this.textBoxAppName.Name = "textBoxAppName";
+            this.textBoxAppName.Size = new System.Drawing.Size(141, 22);
+            this.textBoxAppName.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(32, 53);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(92, 16);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Module Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(32, 101);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(72, 16);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "App Name";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.textBoxAppName);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.textBoxModuleName);
+            this.panel1.Location = new System.Drawing.Point(362, 24);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(350, 499);
+            this.panel1.TabIndex = 6;
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(277, 450);
+            this.ClientSize = new System.Drawing.Size(802, 554);
             this.Controls.Add(this.btnOff);
             this.Controls.Add(this.btnOn);
+            this.Controls.Add(this.panel1);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Form1";
             this.Text = "Switch";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -67,6 +123,11 @@
 
         private System.Windows.Forms.Button btnOn;
         private System.Windows.Forms.Button btnOff;
+        private System.Windows.Forms.TextBox textBoxModuleName;
+        private System.Windows.Forms.TextBox textBoxAppName;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel panel1;
     }
 }
 
