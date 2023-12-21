173394cb831201a1e0d07e01b49ed88965f7799c
DiogoLeonardoGit <dalexandre02@gmail.com>
2023-12-21 19:17:52 +0000

    Valvula a funcionar estatica, imagem som e funcionamento base

---
+++ Valvula/Models/Models/Container.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Valvula.Models
+{
+    public class Container
+    {
+        public string Res_type { get; set; }
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Creation_dt { get; set; }
+        public int Parent { get; set; }
+    }
+
+}
